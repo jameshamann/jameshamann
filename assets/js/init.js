745442a7a06db6f2e863284b745a8ff9a3acfdc8
James Hamann <jameshamann@me.com>
2017-12-06 19:30:31 +0000

    updates theme

---
+++ assets/js/init.js
@@ -0,0 +1,13 @@
+ (function($){
+   $(function(){
+
+     $('.button-collapse').sideNav();
+     $('.parallax').parallax();
+     $('.collapsible').collapsible();
+     $('.carousel.carousel-slider').carousel({fullWidth: true});
+     $('.materialboxed').materialbox();
+     $('.scrollspy').scrollSpy();
+
+
+   }); // end of document ready
+ })(jQuery);
