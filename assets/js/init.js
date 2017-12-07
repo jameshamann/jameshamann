32d1774a63a89e2de26e68ea035c232cda49870f
James Hamann <jameshamann@me.com>
2017-12-07 17:33:41 +0000

    adds google-analytics

--- assets/js/init.js
+++ assets/js/init.js
@@ -7,7 +7,12 @@
      $('.carousel.carousel-slider').carousel({fullWidth: true});
      $('.materialboxed').materialbox();
      $('.scrollspy').scrollSpy();
+     $('.tap-target').tapTarget('open');
 
 
+
+     jQuery('#cookies').on('click', function(event) {
+            jQuery('#cookies').toggle('hide');
+       });
    }); // end of document ready
  })(jQuery);
